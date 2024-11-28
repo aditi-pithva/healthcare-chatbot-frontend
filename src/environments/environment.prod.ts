0559e363a889b7999376f036fb692e3137a67132
Aditi Vaghela <vagheladiti298@gmail.com>
2024-11-28 04:10:55 -0500

    environment.prod.ts changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    production: false,
+    production: true,
     rasaApiUrl: 'http://mh-chatbot-backend.eastus-01.azurecontainer.io:5005/webhooks/rest/webhook',
   };
